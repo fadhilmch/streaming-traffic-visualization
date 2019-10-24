8bac82e2258939d2457b98727967a4019ebd33f9
Fadhilmch <fadhil.mochammad1095@gmail.com>
2019-10-24 16:26:58 +0200

    done with Twitter Streaming using pyspark

--- TwitterStreaming.py
+++ TwitterStreaming.py
@@ -1,38 +1,75 @@
-import pykafka
+#!/usr/bin/python
+
+import sys
 import json
+import pykafka
 import tweepy
 from tweepy import OAuthHandler, Stream
 from tweepy.streaming import StreamListener
+
 import credential
 
-#TWITTER API AUTH
-auth = OAuthHandler(credential.TWITTER_CONSUMER_KEY[0], credential.TWITTER_CONSUMER_SECRET[0])
-auth.set_access_token(credential.TWITTER_TOKEN[0], credential.TWITTER_TOKEN_SECRET[0])
+# # Class for Twitter Streamer
+# class TwitterStreamer():
+#     def __init__(self):
+#         with open('credential.json') as f:
+#             data = json.load(f)
+#             self.api_key = data['twitter_api_key']
+#             self.api_secret = data['twitter_api_secret']
+#             self.token = data['twitter_token']
+#             self.token_secret = data['twitter_token_secret']
+    
+#     def stream_tweets(self, word_filter, language_filter):
+#         #TWITTER API AUTH
+#         auth = OAuthHandler(self.api_key, self.api_secret)
+#         auth.set_access_token(self.token, self.token_secret)
 
-api = tweepy.API(auth)
+#         api = tweepy.API(auth)
+#         twitter_stream = Stream(auth, KafkaPushListener())
+#         twitter_stream.filter(languages=language_filter, track=word_filter)
+        
 
-#Twitter Stream Listener
+
+# Class for Kafka Push Listener
 class KafkaPushListener(StreamListener):          
     def __init__(self):
+        print("Publish data to topic: "+topic)
         self.client = pykafka.KafkaClient("localhost:9092")
 
         #Get Producer that has topic name is Twitter
-        self.producer = self.client.topics[bytes("tweet1", "ascii")].get_producer()
+        self.producer = self.client.topics[bytes(topic, "ascii")].get_producer()
   
     def on_data(self, data):
         #Producer produces data for consumer
-        #Data comes from Twitter
         self.producer.produce(bytes(data, "ascii"))
         print(len(data))
-        print(data)
         return True
                                                                                                                                            
     def on_error(self, status):
         print(status)
         return True
-    
-#Twitter Stream Config
-twitter_stream = Stream(auth, KafkaPushListener())
 
-#Produce Data that has Game of Thrones hashtag (Tweets)
-twitter_stream.filter(track=['jakarta'])
+
+if __name__ == '__main__':
+    topic = sys.argv[1]
+    word_filter = ['jakarta']
+    language_filter = ['en']
+    
+#     twitter_streamer = TwitterStreamer()
+#     twitter_streamer.stream_tweets(language_filter, word_filter)
+    
+    with open('credential.json') as f:
+            data = json.load(f)
+            api_key = data['twitter_api_key']
+            api_secret = data['twitter_api_secret']
+            token = data['twitter_token']
+            token_secret = data['twitter_token_secret']
+            
+    auth = OAuthHandler(api_key, api_secret)
+    auth.set_access_token(token, token_secret)
+
+    api = tweepy.API(auth)
+    twitter_stream = Stream(auth, KafkaPushListener())
+    twitter_stream.filter(languages=language_filter, track=word_filter)
+
+    
